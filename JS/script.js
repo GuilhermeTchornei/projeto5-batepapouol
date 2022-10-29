f75e9bebd3cfd55cc9c63206b65f19749b95e1d0
Guilherme Tchornei <guilhermetchornei@gmail.com>
2022-10-29 17:19:54 -0300

    Projeto completo (sem bônus)

--- JS/script.js
+++ JS/script.js
@@ -1,60 +1,57 @@
-const user = { name: prompt("Qual seu lindo nome?") };
-let messages = [];
+let user;
 const mainContainer = document.querySelector("main");
+const input = document.querySelector("textarea");
 
 enterRoom();
 function enterRoom() {
+    user = { name: prompt("Qual seu lindo nome?") };
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", user);
     promise.then(connected);
     promise.catch(connectionError);
 }
 
-function connected(response) {
-    const connectionMessage = {
-        from: user.name,
-        to: "Todos",
-        text: "entra na sala...",
-        type: "status"
-    };
-    console.log(connectionMessage);
-    //let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", connectionMessage);
-    //promise.then(conectado);
-    //propromisemisse.catch(erro);
-
+function connected() {
     setInterval(keepConnection, 5000);
+    getMessages();
     setInterval(getMessages, 3000);
 }
 
-function conectado(response) {
-    alert("Conectado" + response.status);
-}
-function erro(response) {
-    alert(erro.response.status);
-}
 
 function connectionError(error) {
     if (error.response.status == 400)
     {
-        alert("Nome inváido, tente novamente");
+        alert("Nome em uso, tente outro nome");
+
+        enterRoom();
+    }
+    else
+    {
+        alert("Sua conexão com o servidor foi perdida. A página irá atualizar automaticamente!");
+        window.location.reload();
     }
 }
 
 function keepConnection() {
     const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", user);
-    promise.catch(endConnection);
+    promise.catch(connectionError);
 }
 
 function getMessages() {
     const promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
     promise.then(function (response) {
-        const newMessages = response.data;
-        for (let i = messages.length; i < newMessages.length; i++)
+        mainContainer.innerHTML = '';
+        let messages = response.data;
+        for (let i = 0; i < messages.length; i++)
         {
-            messages.push(newMessages[i]);
             showMessage(messages[i]);
         }
+        focusLastMessage();
     });
+}
 
+function focusLastMessage() {
+    lastMessage = document.querySelector("main").lastChild;
+    lastMessage.scrollIntoView();
 }
 
 function showMessage(message) {
@@ -72,17 +69,27 @@ function showMessage(message) {
             <p><span class="time">${message.time}</span> <span class = "name">${message.from}</span> para <span class = "name">${message.to}</span>: ${message.text}</p>
         </div>`;
     }
-    else
+    else if(message.to === user.name)
     {
         mainContainer.innerHTML += `
         <div class="${message.type}">
             <p><span class="time">${message.time}</span> <span class = "name">${message.from}</span> reservadamente para <span class = "name">${message.to}</span>: ${message.text}</p>
         </div>`;
     }
-
-
 }
 
-function endConnection(error) {
+function sendMessage() {
+    if (input.value === '') return;
+    const message = {
+        from: user.name,
+        to: "Todos",
+        text: input.value,
+        type: "message"
+    };
 
+    const promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", message);
+    promise.then(getMessages);
+    promise.catch(connectionError);
+
+    input.value = '';
 }
